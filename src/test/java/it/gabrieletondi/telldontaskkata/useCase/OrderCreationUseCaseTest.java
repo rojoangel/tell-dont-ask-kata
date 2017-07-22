535c82edb9e648a91d9171501a86a553015e0a78
Angel Rojo Marijuan <arojomarijuan@magento.com>
2017-07-22 23:19:39 +0200

    encapsulate product

--- src/test/java/it/gabrieletondi/telldontaskkata/useCase/OrderCreationUseCaseTest.java
+++ src/test/java/it/gabrieletondi/telldontaskkata/useCase/OrderCreationUseCaseTest.java
@@ -25,16 +25,8 @@ public class OrderCreationUseCaseTest {
     private Category food = new Category("food", new BigDecimal("10"));
     private final ProductCatalog productCatalog = new InMemoryProductCatalog(
             Arrays.<Product>asList(
-                    new Product() {{
-                        setName("salad");
-                        setPrice(new BigDecimal("3.56"));
-                        setCategory(food);
-                    }},
-                    new Product() {{
-                        setName("tomato");
-                        setPrice(new BigDecimal("4.65"));
-                        setCategory(food);
-                    }}
+                    new Product("salad", new BigDecimal("3.56"), food),
+                    new Product("tomato", new BigDecimal("4.65"), food)
             )
     );
     private final OrderCreationUseCase useCase = new OrderCreationUseCase(orderRepository, productCatalog);

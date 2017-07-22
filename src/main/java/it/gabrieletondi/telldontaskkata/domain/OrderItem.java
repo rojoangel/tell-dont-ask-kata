6f298940c2165ec76324650c2f0f27dc6b4e4c1c
Angel Rojo Marijuan <arojomarijuan@magento.com>
2017-07-22 23:03:26 +0200

    add equals and toString to order item

--- src/main/java/it/gabrieletondi/telldontaskkata/domain/OrderItem.java
+++ src/main/java/it/gabrieletondi/telldontaskkata/domain/OrderItem.java
@@ -49,4 +49,37 @@ public class OrderItem {
     private BigDecimal calculateUnitaryTax() {
         return getProduct().getPrice().divide(valueOf(100)).multiply(product.getCategory().getTaxPercentage()).setScale(2, HALF_UP);
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        OrderItem orderItem = (OrderItem) o;
+
+        if (quantity != orderItem.quantity) return false;
+        if (product != null ? !product.equals(orderItem.product) : orderItem.product != null) return false;
+        if (taxedAmount != null ? !taxedAmount.equals(orderItem.taxedAmount) : orderItem.taxedAmount != null)
+            return false;
+        return tax != null ? tax.equals(orderItem.tax) : orderItem.tax == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = product != null ? product.hashCode() : 0;
+        result = 31 * result + quantity;
+        result = 31 * result + (taxedAmount != null ? taxedAmount.hashCode() : 0);
+        result = 31 * result + (tax != null ? tax.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "OrderItem{" +
+                "product=" + product +
+                ", quantity=" + quantity +
+                ", taxedAmount=" + taxedAmount +
+                ", tax=" + tax +
+                '}';
+    }
 }
